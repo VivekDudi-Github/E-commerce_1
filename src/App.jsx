2133775ca0b928c6e21ab6ada56788be6bd74788
VivekDudi-Github <vivekdudi18@gmail.com>
2024-07-06 16:54:16 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import { Button } from '@material-tailwind/react'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Button>Click</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,19 @@
+import Navbar from "../navbar/Navbar"
+import Footer from "../footer/Footer"
+
+
+import React from 'react'
+
+function Layout({children}) {
+  return (
+    <div>
+        <Navbar/>
+        <div className='main-content '>
+            {children}
+        </div>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Layout
