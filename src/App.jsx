07cf72b59308d817673893d9ed828ef14c4ced46
VivekDudi-Github <vivekdudi18@gmail.com>
2024-07-07 17:53:57 +0530

    Added react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import { useState } from 'react'
 import './App.css'
 import { Button } from '@material-tailwind/react'
+import {   BrowserRouter ,  Routes , Route} from "react-router-dom"
+import Home from './components/pages/home/Home'
+import No_home from './components/pages/no home/No_home'
 
 function App() {
 
   return (
     <>
       <div>
-        <Button>Click</Button>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/*' element={<No_home/>} />
+          </Routes>
+        </BrowserRouter>
       </div>
     </>
   )
---
+++ src/components/pages/no home/No_home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function No_home() {
+  return (
+    <div>No_home</div>
+  )
+}
+
+export default No_home
